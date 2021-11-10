6d4cfe449bb831c8106fd66f1e267e0333bd6f86
ossoni <ahnssoni@gmail.com>
2021-11-10 11:11:56 +0900

    로그인 페이지 완선

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-//상단 Search 인풋 
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function() {
-    //logic..
-    searchInputEl.focus();
-});
-searchInputEl.addEventListener('focus', function () {
-    searchEl.classList.add('focused')
-    searchInputEl.setAttribute('placeholder','통합검색');
-});
-searchInputEl.addEventListener('blur', function () {
-    searchEl.classList.remove('focused')
-    searchInputEl.setAttribute('placeholder','');
-});
 
 //배지 사라짐
 const badgeEl = document.querySelector('header .badges');
@@ -148,8 +132,4 @@ spyEls.forEach(function(spyEl) {
         .setClassToggle(spyEl, 'show')
         .addTo(new ScrollMagic.Controller());
 });
-
-// 년도 자동 삽입
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
 
